3374d39a1ae1504914472de3ea5dd318da72d474
ameerthehacker <ameerjhanprof@gmail.com>
2017-10-08 22:15:48 +0530

    Perform DCT on the prepared macro blocks

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const fs = require('fs');
 const jpeg = require('./jpeg');
 
 getPixels('./images/lena.png', 'image/png', (err, pixels) => {
-    console.log(jpeg(pixels.data));
+  let width = pixels.shape[0];
+  let height = pixels.shape[1];
+  console.log(jpeg(pixels.data, width, height));
+  // jpeg(pixels.data, width, height);
 });
--- jpeg.js
+++ jpeg.js
@@ -15,16 +15,53 @@ function getRgbBlocks(pixels) {
   return rgbBlocks;
 }
 
-function prepareBlocks(pixels) {
+function prepareBlocks(pixels, width, height) {
   // Get the rgb blocks
   rgbBlocks = getRgbBlocks(pixels);
   // Convert rgbBlocks to yiqBlocks
-  yiqBlocks = [];
-  for(i = 0;i < rgbBlocks.length; i++) {
+  let yiqBlocks = [];
+  for(let i = 0; i < rgbBlocks.length; i++) {
     yiqBlock = rgb2yiq(rgbBlocks[i]);
     yiqBlocks.push(yiqBlock);
   }
-  return yiqBlocks
+  // Seperate the pixles into 8 * 8 blocks
+  let noBlocks = yiqBlocks.length / (height * 8);
+  let macroBlocks = [];
+  for(let i = 0; i < noBlocks; i++) {
+    blockIndex = i * 8;
+    let macroBlock = [];
+    for(let row = blockIndex; row < blockIndex + 8; row++) {
+      let macroBlockRow = [];
+      for(let col = blockIndex; col < blockIndex + 8; col++) {
+        macroBlockRow.push(yiqBlocks[ row * width + col  ]);
+      }
+      macroBlock.push(macroBlockRow);
+    }
+    macroBlocks.push(macroBlock);
+  }
+  return macroBlocks;
+}
+function DCT(macroBlocks) {
+  // Iterate through each macro block
+  for(let i = 0; i < macroBlocks.length; i++){
+    for(let j = 0; j < 8; j++) {
+      for(let k = 0; k < 8; k++) {
+        // Iterate through each color code
+        for(let l = 0; i < 3; i++) {
+          let a = macroBlocks[i][j][k][l];
+          macroBlocks[i][j][k][l] = a * Math.cos((2 * Math.PI * (2 * j + 1)) / 16) * Math.cos((2 * Math.PI * (2 * k + 1)) / 16);
+        }
+      }
+    }
+  }
+  return macroBlocks;
+}
+function compress(pixels, height, width) {
+  // Prepare the macroblocks
+  let macroBlocks = prepareBlocks(pixels, height, width);
+  // Perform DCT on the macroblocks
+  macroBlocks = DCT(macroBlocks);
+  return macroBlocks;
 }
 
-module.exports = prepareBlocks;
+module.exports = compress;
