aa19ba21a6a9a9e397c2460041d36a4f05735066
ameerthehacker <ameerjhanprof@gmail.com>
2017-10-16 00:54:18 +0530

    Add rle encoding decoding and jpeg decoding

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const jpeg = require('./jpeg');
 getPixels('./images/lena.png', 'image/png', (err, pixels) => {
   let width = pixels.shape[0];
   let height = pixels.shape[1];
-  console.log(jpeg(pixels.data, width, height));
+  console.log("Encoding the jpeg image");
+  encodedImage = jpeg.encode(pixels.data, height, width);
+  console.log(encodedImage);
+  console.log("Decoding the jpeg image");
+  decodedImage = jpeg.decode(encodedImage, height, width);
+  console.log(decodedImage);
 });
--- jpeg.js
+++ jpeg.js
@@ -1,5 +1,3 @@
-const rle = require('rle');
-
 function rgb2yiq(rgb) {
   yiq = [0, 0, 0]
   yiq[0] = (0.299 * rgb[0]) + (0.587 * rgb[1]) + (0.144 * rgb[2]);
@@ -50,6 +48,18 @@ function prepareBlocks(pixels, width, height) {
   }
   return macroBlocks;
 }
+function seperateBlocks(macroBlocks, height, width) {
+  let array = [];
+  for(let i = 0; i < 8; i++) {
+    for(j = 0; j < macroBlocks.length; j++) {
+      let macroBlockRow = macroBlocks[j].pop();
+      for(k = 0; k < 8; k++) {
+        array.push(macroBlockRow.pop());
+      }
+    }
+  }
+  return array;
+}
 function DCT(macroBlocks) {
   // Iterate through each macro block
   for(let i = 0; i < macroBlocks.length; i++){
@@ -62,6 +72,23 @@ function DCT(macroBlocks) {
         // a => Amplitude of DCT
         let a = macroBlocks[i][j][k];
         macroBlocks[i][j][k] = a * Math.cos((2 * Math.PI * j * (2 * j + 1)) / 16) * Math.cos((2 * k * Math.PI * (2 * k + 1)) / 16);
+      }
+    }
+  }
+  return macroBlocks;
+}
+function inverseDCT(macroBlocks) {
+  // Iterate through each macro block
+  for(let i = 0; i < macroBlocks.length; i++){
+    for(let j = 0; j < 8; j++) {
+      for(let k = 0; k < 8; k++) {
+        // Iterate through each color channel
+        // i => Index of the current macro block being processed
+        // j => Index of the current row of the macro block being processed
+        // k => Index of the current column of the macro block being processed
+        // a => Amplitude of DCT
+        let a = macroBlocks[i][j][k];
+        macroBlocks[i][j][k] = a * (1.414 / 7) * Math.cos((2 * Math.PI * j * (2 * j + 1)) / 16) * Math.cos((2 * k * Math.PI * (2 * k + 1)) / 16);
       }
     }
   }
@@ -94,6 +121,33 @@ function quantize(macroBlocks) {
   }
   return macroBlocks;
 }
+function deQuantize(macroBlocks) {
+  //  Create the quantization matrix
+  let quantizationMatrix = [
+    [16, 11, 10, 16, 24, 40, 51, 61],
+    [12, 12, 14, 19, 26, 58, 60, 55],
+    [14, 13, 16, 24, 40, 57, 69, 56],
+    [14, 17, 22, 29, 51, 87, 80, 62],
+    [18, 22, 37, 56, 68, 109, 103, 77],
+    [24, 35, 55, 64, 81, 104, 113, 92],
+    [49, 64, 78, 87, 103, 121, 120, 101],
+    [72, 92, 95, 98, 112, 100, 103, 99]
+  ];
+  // Iterate through each macro block
+  for(let i = 0; i < macroBlocks.length; i++){
+    for(let j = 0; j < 8; j++) {
+      for(let k = 0; k < 8; k++) {
+        // Iterate through each color code
+        // i => Index of the current macro block being processed
+        // j => Index of the current row of the macro block being processed
+        // k => Index of the current column of the macro block being processed
+        let quantizationValue = quantizationMatrix[j][k];
+        macroBlocks[i][j][k] = macroBlocks[i][j][k] * quantizationValue;
+      }
+    }
+  }
+  return macroBlocks;
+}
 function zigZagScan(macroBlocks) {
   let array = [];
   for(let i = 0; i < macroBlocks.length; i++) {
@@ -106,11 +160,80 @@ function zigZagScan(macroBlocks) {
     // ZigZag for bottom row
     for(let j = 0; j < 8; j++) {
       for(let k = 7, l = 0; k >= j; k--, l++) {
-        array.push(macroBlocks[i][k][l]);
+        array.push(macroBlocks[i][k][l + j]);
       }
     }
   }
   return array;
+}
+function inverseZigZagScan(array) {
+  let macroBlocks = [];
+  for(let i = 0; i < array.length;) {
+    // Allocate a new macro block of size 8 * 8
+    let macroBlock = new Array(8);
+    for(let l = 0; l < macroBlock.length; l++) {
+      macroBlock[l] = new Array(8);
+    }
+    // ZigZag for right column
+    for(let j = 0; j < 7; j++) {
+      for(let k = 0; k <= j; k++) {
+        macroBlock[j - k][k] = array[i];
+        i++;
+      }
+    }
+    // ZigZag for bottom row
+    for(let j = 0; j < 8; j++) {
+      for(let k = 7, l = 0; k >= j; k--, l++) {
+        macroBlock[k][l + j] = array[i];
+        i++;
+      }
+    }
+    macroBlocks.push(macroBlock);
+  }
+  return macroBlocks;
+}
+function rleEncode(array, threshold) {
+  let buffer = "";
+  let count = 1;
+  for(let i = 0; i < array.length;) {
+    if(i + 1 < array.length && array[i] == array[i + 1]) {
+      i++;
+      count++;
+    }
+    else {
+      if(count > threshold) {
+        buffer += array[i] + "$" + count + ":";
+      }
+      else {
+        for(let j = 0; j < count; j++) {
+          buffer += array[i] + ":";
+        }
+      }
+      count = 1;
+      i++;
+    }
+  }
+  // Remove trailing colon(:)
+  buffer = buffer.substr(0, buffer.length - 1);
+  return buffer;
+}
+function rleDecode(str, threshold) {
+  let array = str.split(':');
+  let decodedArray = [];
+  for(let i = 0; i < array.length; i++) {
+    let tmp = array[i].split('$');
+    if(tmp.length == 2) {
+      let element = tmp[0];
+      let count = tmp[1];
+      for(let j = 0; j < count; j++) {
+        decodedArray.push(element);
+      }
+    }
+    else {
+      decodedArray.push(array[i]);
+    }
+  }
+  return decodedArray;
 }
 function encode(pixels, height, width) {
   // Prepare the macroblocks
@@ -120,9 +243,28 @@ function encode(pixels, height, width) {
   // Quantize the macroblock using the quantization matrix
   macroBlocks = quantize(macroBlocks);
   // Perform zig zag scan
-  macroBlocks = zigZagScan(macroBlocks);
+  array = zigZagScan(macroBlocks);
+  // Perform RLE encoding
+  array = rleEncode(array, 2);
 
-  return macroBlocks;
+  return array;
+}
+function decode(array, height, width) {
+  // Expand the array
+  array = rleDecode(array);
+  // Inverse ZigZag scan
+  let macroBlocks = inverseZigZagScan(array);
+  // Dequantize the macroblocks
+  macroBlocks = deQuantize(macroBlocks);
+  // Inverse DCT
+  macroBlocks = inverseDCT(macroBlocks);
+  // Seperate the blocks into 1D array
+  let pixels = seperateBlocks(macroBlocks, height, width);
+  return pixels;
 }
 
-module.exports = encode;
+
+module.exports = {
+  'encode': encode,
+  'decode': decode
+};
